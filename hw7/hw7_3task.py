d550e227addffa716b9546b0247ac5a469b66bb1
mariko-ua <141179819+mariko-ua@users.noreply.github.com>
2023-08-27 16:32:40 +0300

    Update hw7_3task.py

--- hw7/hw7_3task.py
+++ hw7/hw7_3task.py
@@ -2,6 +2,10 @@
 # [1, 2, 5, 7, 9, 99, 87, 200, 39, 2]
 # [5, 42, 29, 345, 50, 33, 7, 0, 201, 9, 2, 132, 45, 23,934]
 # Порахувати скільки разів числа з одного списку зустрічаються у ішому списку. Вивести у вигляді відформатованої таблиці.
+#Upd:
+# тут знайдено скільки елементів одного списку є у іншому списку
+# додайте теж саме тільки навпаки
+# скільки алементів другого списку входять також до першо списку
 
 first_list = [1, 2, 5, 7, 9, 99, 87, 200, 39, 2]
 second_list = [5, 42, 29, 345, 50, 33, 7, 0, 201, 9, 2, 132, 45, 23,934]
@@ -9,12 +13,24 @@ first_list.sort()
 second_list.sort()
 print(first_list)
 print(second_list)
-third_list = []
+
 a = 0
-for common in first_list:
-    if common in second_list:
-        third_list.append(common)
+
+print("Співпадіння елементів з першого списку у другому:")
+print("{:<15} {:<15}".format("Ідекс елементу", "Елемент"))
+for common_f in first_list:
+    if common_f in second_list:
+        print("{:<15} {:<15}".format(first_list.index(common_f), common_f)) # тут є проблема з індексом двійки. не розумію як вирішити проблему
         a += 1
-third_list.sort()
-print(third_list)
+print("Всього: ", a)
+
+b = 0
+print("Співпадіння елементів з другого списку у першому:")
+print("{:<15} {:<15}".format("Ідекс елементу", "Елемент"))
+for common_s in second_list:
+    if common_s in first_list:
+        print("{:<15} {:<15}".format(second_list.index(common_s), common_s))
+        b += 1
+print("Всього: ", b)
+
 
